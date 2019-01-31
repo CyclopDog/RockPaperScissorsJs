fe0506e0aab955456d19d2d900939878676c4f91
Marcos Fernandes <contato@marcos-fernandes.com>
2019-01-31 09:02:03 -0200

    updated with more js code and style

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 const buttons = document.querySelectorAll('button');
 const outcome = document.querySelector('#outcome');
+const rounds = document.querySelector('#rounds');
+const win = document.querySelector('#win');
+const lose = document.querySelector('#lose');
+
+let lifes = 5;
+let wins = 0;
+let loses = 0;
 
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
         let playerSelection = button.id;
         let compSelection = compSelect();
         playRound(playerSelection, compSelection);
+        rounds.textContent = `Lifes: ${lifes}`;
+        win.textContent = `Wins: ${wins}`;
+        lose.textContent = `Loses: ${loses}`;
+        if (lifes === 0) {
+            if (wins > loses) {
+                outcome.textContent = 'Congratulations, you won the match!';
+            } else if (loses > wins) {
+                outcome.textContent = 'You have lost the match!';
+            } else {
+                outcome.textContent = 'The match was drawn!';
+            }
+            lifes = 5;
+            wins = 0;
+            loses = 0;
+        }
     });
   });
-  
+
+function compSelect() {
+    let compPlay = ['rock', 'paper', 'scissors'];
+    let compMove = compPlay[Math.floor(Math.random()*compPlay.length)];
+    return compMove;
+}
+
 function playRound(pMove, cMove) {
     if (pMove === 'rock') {
         switch (cMove) {
@@ -17,9 +45,12 @@ function playRound(pMove, cMove) {
             break;
             case 'paper':
                 outcome.textContent = 'Computer played Paper, you lost!'
+                loses += 1;
+                lifes -= 1;
             break;
             case 'scissors':
                 outcome.textContent = 'Computer played Scissors, you won!'
+                wins += 1;
             break;
         }
     } else if (pMove === 'paper') {
@@ -29,9 +60,12 @@ function playRound(pMove, cMove) {
             break;
             case 'scissors':
                 outcome.textContent = 'Computer played Scissors, you lost!'
+                loses += 1;
+                lifes -= 1;
             break;
             case 'rock':
                 outcome.textContent = 'Computer played Rock, you won!'
+                wins += 1;
             break;
         }
     } else if (pMove === 'scissors') {
@@ -41,19 +75,16 @@ function playRound(pMove, cMove) {
             break;
             case 'rock':
                 outcome.textContent = 'Computer played Rock, you lost!'
+                loses += 1;
+                lifes -= 1;
             break;
             case 'paper':
                 outcome.textContent = 'Computer played Paper, you won!'
+                wins += 1;
             break;
         }
     }
 
     return;
     
-}
-
-function compSelect() {
-    let compPlay = ['rock', 'paper', 'scissors'];
-    let compMove = compPlay[Math.floor(Math.random()*compPlay.length)];
-    return compMove;
 }
